855b24576b0cf93ef1eccfc7b3aa9067b5811e75
Lemlem-yo <ltsifete@gmail.com>
2023-10-23 14:30:49 +0300

    correction on the betty file of the bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * bubble_sort: sort the seeing the adjacent of the array
+ * bubble_sort - function sort the seeing the adjacent of the array
  * @array: pointer for the array
- * size : the size of the array hold
+ * @size : the size of the array hold
  * Return: None
  */
 void bubble_sort(int *array, size_t size)
@@ -12,31 +12,23 @@ void bubble_sort(int *array, size_t size)
 	int swapped;
 	size_t i;
 	size_t j;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		swapped = 0;
+
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				int tmp = array[j];
+
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				swapped = 1;
 			}
 		}
-
-		if (!swapped) break;
+		if (!swapped)
+			break;
 	}
-}
-/**
- * swap: function to swap the function
- * @x: pointer for the swap
- * @y: pointer for the swap
- */
-void swap(int *x, int *y)
-{
-	int tmp = *x;
-	*x = *y;
-	*y = tmp;
 }
